7ed5016becd4524ccc7dbe86efbb4095b73a81bd
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-12-02 13:05:43 +0200

    Added vue router and created Home and About components removed app.js from repo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,13 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue';
+import VueRouter from 'vue-router'
+import routes from './routes'
+
+Vue.use(VueRouter)
+
+
+window.Vue = Vue;
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,4 +30,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+
+    router: new VueRouter(routes)
 });
